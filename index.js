a9e6fef564df362907d2503ae2ba661223dfc7e9
Yatendra Chauhan <yatendra.chauhan@nagarro.com>
2023-06-12 13:32:58 +0530

    Update indx.js

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB
 mongoose.connect('mongodb://35.188.100.25:27017/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 // Create an Express app
 const app = express();
+app.get('/', (req, res) => {
+    res.send('Demo app for testing docker and kubernates on GCP. ${port}');
+});
 
 // Define your routes and middleware here
 
 // Start the server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
