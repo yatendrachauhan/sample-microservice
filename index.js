b43efd0e5fe7a60d5ca7bb16c1e31d310bd73334
Yatendra Chauhan <yatendra.chauhan@nagarro.com>
2023-06-12 14:58:32 +0530

    Update Http request in index.js

--- index.js
+++ index.js
@@ -15,8 +15,47 @@ mongoose.connect('mongodb://35.188.100.25:27017/mydatabase', {
 
 // Create an Express app
 const app = express();
+
+// Create a collection (if not already created)
+app.post('/api/collection', (req, res) => {
+    const collectionName = req.body.collectionName;
+    const collection = mongoose.connection.db.collection(collectionName);
+    console.log(`Using collection: ${collectionName}`);
+
+    res.status(200).json({ message: `Using collection: ${collectionName}` });
+});
+
+// Insert documents
+app.post('/api/insert', (req, res) => {
+    const collectionName = req.body.collectionName;
+    const documents = req.body.documents;
+
+    const collection = mongoose.connection.db.collection(collectionName);
+    collection.insertMany(documents)
+        .then(() => {
+            res.status(200).json({ message: 'Documents inserted' });
+        })
+        .catch((error) => {
+            res.status(500).json({ error: 'Error inserting documents' });
+        });
+});
+
+// Fetch records
+app.get('/api/fetch/:collectionName', (req, res) => {
+    const collectionName = req.params.collectionName;
+
+    const collection = mongoose.connection.db.collection(collectionName);
+    collection.find().toArray()
+        .then((documents) => {
+            res.status(200).json(documents);
+        })
+        .catch((error) => {
+            res.status(500).json({ error: 'Error fetching documents' });
+        });
+});
+
 app.get('/', (req, res) => {
-    res.send('Demo app for testing docker and kubernates on GCP. ${port}');
+    res.send(`Sample microservice app that will create a collection and provide facility to insert and fetch records. ${port}`);
 });
 
 // Define your routes and middleware here
