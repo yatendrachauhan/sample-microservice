0312e3d0d5d24fbe593630e61b14eed2fd731e15
Yatendra Chauhan <yatendra.chauhan@nagarro.com>
2023-06-15 10:40:19 +0530

    Remove unused code

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 // Connect to MongoDB
 const pEnv = process.env;
-const mongodbUri = (pEnv.MONGO_USER && pEnv.MONGO_HOST) ? 
-        `mongodb://${pEnv.MONGO_USER}:${pEnv.MONGO_PASS}@${pEnv.MONGO_HOST}:${pEnv.MONGO_PORT}/${pEnv.MONGO_DB}?authSource=admin` :
-        "mongodb://localhost:27017/mydatabase";
+const mongodbUri = (pEnv.MONGO_USER && pEnv.MONGO_HOST) ?
+    `mongodb://${pEnv.MONGO_USER}:${pEnv.MONGO_PASS}@${pEnv.MONGO_HOST}:${pEnv.MONGO_PORT}/${pEnv.MONGO_DB}?authSource=admin` :
+    "mongodb://localhost:27017/mydatabase";
 mongoose.connect(mongodbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -25,12 +25,15 @@ app.use(express.json());
 
 // Create a collection (if not already created)
 app.post('/api/collection', (req, res) => {
-    console.log(req);
     const collectionName = req.body.collectionName;
-    const collection = mongoose.connection.db.collection(collectionName);
-    console.log(`Using collection: ${collectionName}`);
+    if (mongoose.connection) {
+        const collection = mongoose.connection.db.collection(collectionName);
+        console.log(`Using collection: ${collectionName}`);
 
-    res.status(200).json({ message: `Using collection: ${collectionName}` });
+        res.status(200).json({ message: `Using collection: ${collectionName}` });
+    } else {
+        res.status(500).json({ error: 'DB connection not available' });
+    }
 });
 
 // Insert documents
@@ -38,32 +41,40 @@ app.post('/api/insert', (req, res) => {
     const collectionName = req.body.collectionName;
     const documents = req.body.documents;
 
-    const collection = mongoose.connection.db.collection(collectionName);
-    collection.insertMany(documents)
-        .then(() => {
-            res.status(200).json({ message: 'Documents inserted' });
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Error inserting documents' });
-        });
+    if (mongoose.connection) {
+        const collection = mongoose.connection.db.collection(collectionName);
+        collection.insertMany(documents)
+            .then(() => {
+                res.status(200).json({ message: 'Documents inserted' });
+            })
+            .catch((error) => {
+                res.status(500).json({ error: 'Error inserting documents' });
+            });
+    } else {
+        res.status(500).json({ error: 'DB connection not available' });
+    }
 });
 
 // Fetch records
 app.get('/api/fetch/:collectionName', (req, res) => {
     const collectionName = req.params.collectionName;
 
-    const collection = mongoose.connection.db.collection(collectionName);
-    collection.find().toArray()
-        .then((documents) => {
-            res.status(200).json(documents);
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Error fetching documents' });
-        });
+    if (mongoose.connection) {
+        const collection = mongoose.connection.db.collection(collectionName);
+        collection.find().toArray()
+            .then((documents) => {
+                res.status(200).json(documents);
+            })
+            .catch((error) => {
+                res.status(500).json({ error: 'Error fetching documents' });
+            });
+    } else {
+        res.status(500).json({ error: 'DB connection not available' });
+    }
 });
 
 app.get('/', (req, res) => {
-    res.send(`Sample microservice app that will create a collection and provide facility to insert and fetch records. ${port}`);
+    res.send(`Sample microservice app that will create a collection and provide facility to insert and fetch records.`);
 });
 
 // Start the server
